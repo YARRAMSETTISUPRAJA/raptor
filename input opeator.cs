caa35d315e7c8f289ea81225bb590e06ed9132bc
supraja <supruubtech@gmail.com>
2019-09-07 17:07:02 +0530

    raptor basic class

---
+++ input opeator.cs
@@ -0,0 +1,20 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace input opeator
+{
+   public class main_class
+   {
+      static System.Random random_generator = new System.Random();
+      public static void Main(string[] args)
+      {
+         string raptor_prompt_variable_zzyz;
+         ?? b;
+      
+         raptor_prompt_variable_zzyz ="enter the value";
+         Console.WriteLine(raptor_prompt_variable_zzyz);
+         b= Double.Parse(Console.ReadLine());
+      }
+   }
+}
---
+++ output block.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+
+   cout << "supraja" << endl;
+   return 0;
+}
